96c460f3f6572d5a2c6367969a616ad0bab52aef
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2025-05-31 09:09:35 -0300

    refactor(user): improve schema structure and password hashing

--- models/User.js
+++ models/User.js
@@ -7,3 +7,44 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("User", UserSchema);
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "O nome é obrigatório"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "O email é obrigatório"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Por favor, insira um email válido"],
+    },
+    password: {
+      type: String,
+      required: [true, "A senha é obrigatória"],
+      minlength: [6, "A senha deve ter no mínimo 6 caracteres"],
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
