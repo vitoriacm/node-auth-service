056b474adc502c1a859d214889f53eff6a647514
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2025-06-01 22:01:08 -0300

    refactor(controller): delegate auth logic to authService

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,26 @@
-const User = require("../models/User");
+const authService = require("../services/authService");
 
-exports.getUserById = async (req, res) => {
-  const id = req.params.id;
-
+exports.register = async (req, res, next) => {
   try {
-    const user = await User.findById(id, "-password");
+    const { name, email, password, confirmPassword } = req.body;
+    const user = await authService.registerUser(
+      name,
+      email,
+      password,
+      confirmPassword
+    );
+    res.status(201).json({ msg: "Usuário criado com sucesso!", user });
+  } catch (error) {
+    next(error);
+  }
+};
 
-    if (!user) {
-      return res.status(404).json({ msg: "Usuário não encontrado" });
-    }
-
-    return res.status(200).json(user);
-  } catch (err) {
-    console.error("Erro na rota GET /user/:id", err);
-    return res.status(500).json({ msg: "Erro no servidor" });
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const token = await authService.loginUser(email, password);
+    res.status(200).json({ msg: "Autenticado com sucesso", token });
+  } catch (error) {
+    next(error);
   }
 };
